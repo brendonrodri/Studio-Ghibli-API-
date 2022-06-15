c7b0d8181786c97e302d014bc42c5a08ddf6227e
Brendon Rodrigues <rodriguesbrendon861@gmail.com>
2022-06-15 19:49:57 -0300

    iniciando repositório

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components"
 
-function App() {
+const Container = styled.section`
+  width: 100%;
+  div{
+    display: flex;
+    margin: 20px;
+ 
+  }
+  
+  img{
+    width: 20vw;
+    height: 50vh;
+  }
+`
+const FilmeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  p{
+    width: 40%;
+    text-align: justify;
+  }
+
+`
+
+export default function App() {
+  const [film, setFilm] = useState([]);
+  useEffect(()=>{
+    filmes()
+  },[])
+  const filmes = () => {
+    axios.get("https://ghibliapi.herokuapp.com/films").then((response) => {
+      console.log(response.data);
+      setFilm(response.data);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {film.map((item) => (
+        <div>
+          {" "}
+          <img src={item.image} alt={item.title} />
+          <FilmeContainer>
+            <h3>{item.title}</h3>
+            <p>{item.description} </p>
+            <p><b>Ano de lançamento:</b> {item.release_date} </p>
+          </FilmeContainer>
+         
+        </div>
+      ))}
+    </Container>
   );
 }
-
-export default App;
